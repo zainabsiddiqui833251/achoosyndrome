66122a7a3a937516be290aa32a0b89aa90d8887b
Zainab Siddiqui <zs833251@gmail.com>
2024-10-18 01:06:12 +0500

    fourth commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className='w-full max-w-[70%] m-auto h-[30vw] bg-no-repeat bg-contain bg-center'>
-
+    <header 
+      className='w-full max-w-[70%] m-auto h-[30vw] bg-no-repeat bg-cover bg-center' 
+      style={{ backgroundImage: "url('/path/to/your/image.jpg')" }} // Replace with your image path
+    >
     </header>
   );
 }
 
 export default Header;
+
